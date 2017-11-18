35362ead6d08709157b6d64539c628ae79a56b70
Hackbee <hackbee@outlook.com>
2017-11-18 17:25:36 +0800

    basic view

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            Power By <a href="https://github.com/hackbeex" target="_blank">Hackbee</a> <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:hackbee@outlook.com">联系我</a></p>
+    </div>
+</footer>
